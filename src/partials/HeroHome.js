da0ec08edcd319d0a152bc9b09fd017e059e1f0b
GuilhermeDeOliveiraAmorim <guilhermeamorim@deso-se.com.br>
2021-11-25 13:57:07 -0300

    Atualização: conteúdo

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -35,7 +35,7 @@ function HeroHome() {
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">ASD Soluções Digitais</h1>
             <h2 className="text-5xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400" data-aos="zoom-y-out">Soluções digitais para o seu negócio!</h2>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Entre em contato conosco! Podemos ajudar a sua empresa a dar um passo rumo ao mundo digital!</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
